c0f9fb6bb64f9a8d532bd9c6d93d07d017dc1ff5
nabaraj <nabaraj2055@gmail.com>
2021-05-31 15:26:33 +0545

    add copy to clipboard

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import { ColorContext } from "./contexts/ColorContext";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 import "./style/colorBox.css";
 
 const ColorBox = () => {
@@ -9,15 +10,21 @@ const ColorBox = () => {
   return (
     <div className="colors">
       {colors.map((c, i) => (
-        <div className="color_box" key={i} style={{ backgroundColor: c.color }}>
-          <div className="detail-copy">
-            <button>Copy</button>
+        <CopyToClipboard text={c.color}>
+          <div
+            className="color_box"
+            key={i}
+            style={{ backgroundColor: c.color }}
+          >
+            <div className="detail-copy">
+              <button>Copy</button>
+            </div>
+            <div className="detail-name">
+              <h6>{c.name}</h6>
+              <h5>More</h5>
+            </div>
           </div>
-          <div className="detail-name">
-            <h6>{c.name}</h6>
-            <h5>More</h5>
-          </div>
-        </div>
+        </CopyToClipboard>
       ))}
     </div>
   );
